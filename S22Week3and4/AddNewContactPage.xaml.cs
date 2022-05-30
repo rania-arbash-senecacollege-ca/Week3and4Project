b070eb77eb39921f97725840aa65632a94917f79
rania <rania.arbash@senecacollege.ca>
2022-05-30 13:52:44 -0400

    Week4 Project: Adding Navigation and passing data

---
+++ S22Week3and4/AddNewContactPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace S22Week3and4
+{
+    public partial class AddNewContactPage : ContentPage
+    {
+        ContactCollection AddPageContactCollection;
+        public AddNewContactPage(ContactCollection cc)
+        {
+            InitializeComponent();
+            AddPageContactCollection = cc;
+        }
+
+        async void save_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (name.Text != null && numer.Text != null)
+            {
+                
+                Contact c = new Contact(name.Text, numer.Text, "https://images.freeimages.com/images/small-previews/804/tubes-1252172.jpg");
+                AddPageContactCollection.addNewContact(c);
+                await Navigation.PopModalAsync();
+            }
+            else
+                await DisplayAlert("Error", "Missing Info", "OK");
+
+
+        }
+
+       async void cancel_Clicked(System.Object sender, System.EventArgs e)
+        {
+           await Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ S22Week3and4/ContactCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace S22Week3and4
+{
+    public class ContactCollection
+    {
+       public ObservableCollection<Contact> contacts { get; set; }
+
+       public ContactCollection() {
+         contacts = new ObservableCollection<Contact>();
+
+        }
+
+        public void addNewContact(Contact c)
+        {
+            contacts.Add(c);// in real apps, we have to save this Contact someware
+        }
+
+        public void deleteContact(Contact c)
+        {
+            contacts.Remove(c);// in real apps, we have to save this Contact someware
+        }
+    }
+}
